d5417f0b30fd492cec753425070a9868b8ecef2a
Diego Rodrigues <diegorodgs@outlook.com>
2022-07-27 18:53:37 -0300

    Simplificando com eval()

--- js/calc.js
+++ js/calc.js
@@ -1,3 +1,27 @@
+//incluindo eval()
+function calc(tipo,valor){
+    if(tipo == 'func'){
+            if(valor == 'c'){
+                document.getElementById('resultado').value = '';
+            }
+            if((valor == '+') || (valor == '-') || (valor == '*') || (valor == '/') || (valor == '.')){
+                document.getElementById('resultado').value += valor;
+            }
+            if(valor == '='){
+                document.getElementById('resultado').value = (eval(document.getElementById('resultado').value))
+            }
+    }else if(tipo == 'valor'){
+        document.getElementById('resultado').value += valor;
+    }
+}
+
+
+
+
+
+
+/* Sem eval()
+
 let hist = document.getElementById('hist');
 let tela = document.getElementById('resultado');
 let lista = document.getElementById('lista');
@@ -51,4 +75,4 @@ function limpar() {
     lista.innerHTML = '';
     contas.length = 0
     result = 0
-}
+}*/
